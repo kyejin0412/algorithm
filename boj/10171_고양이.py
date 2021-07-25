9c67831dc710f612e93190747d1881e1901b59ba
kyejin0412 <kyejin0412@kookmin.ac.kr>
2021-07-25 23:20:05 +0900

    고양이 다른 방법

--- boj/10171_고양이.py
+++ boj/10171_고양이.py
@@ -1,4 +1,14 @@
+#첫번째 방법
 print('\    /\ ')
 print(" )  ( ')")
 print('(  /  )')
 print(' \(__)|')
+
+
+#두번째 방법
+multi_line_string = """\    /\ 
+ )  ( ')
+(  /  )
+ \(__)|"""
+
+print(multi_line_string)
